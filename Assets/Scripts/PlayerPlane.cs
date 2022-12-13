8167b46eb0fa5068d69301d70c0333cc4895e3c4
Loïc Rohan <loicrohan@gmail.com>
2022-12-13 12:17:54 +0530

    Clumsy Copter Rohan

---
+++ Assets/Scripts/PlayerPlane.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPlane : MonoBehaviour
+{
+    public static int score = 0;
+
+    public float tapForce = 265;
+
+    public Text scoreText;
+
+    public GameObject looseScreen;
+
+    private Rigidbody2D playerRigidbody;
+
+    private AudioSource _audioSource;
+
+    [SerializeField]
+    private AudioClip _clip;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        playerRigidbody = GetComponent<Rigidbody2D>();
+
+        //Reset the score everytime the game restarts
+        score = 0;
+
+        Time.timeScale = 0;
+    }
+
+    void Update()
+    {
+        //The Score text updates every frame
+        scoreText.text = score.ToString();
+
+        if (Input.GetKeyDown(KeyCode.Space) || (Input.GetMouseButtonDown(0)))
+        {
+            _audioSource.Play();
+            playerRigidbody.AddForce(Vector2.up * tapForce);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Rocks")
+        {
+            AudioSource.PlayClipAtPoint(_clip, Camera.main.transform.position, 1f);
+
+            looseScreen.SetActive(true);
+
+            Time.timeScale = 0;
+        }
+    }
+
+    //Called externally
+    public void SetGameTimeScale(float newScale)
+    {
+        Time.timeScale = newScale;
+    }
+}
---
+++ Assets/Scripts/RockSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSpawner : MonoBehaviour
+{
+    public GameObject Rocks;
+    
+    public float frequency = 1;
+   
+    private void Start()
+    {
+        InvokeRepeating("SpawnRocks", 1, frequency);
+    }
+    
+    void SpawnRocks()
+    {
+        Vector3 randomPos = new Vector3(transform.position.x, Random.Range(-0.46f, 2.78f), transform.position.z);
+        
+        Instantiate(Rocks, randomPos, transform.rotation);
+    }
+}
