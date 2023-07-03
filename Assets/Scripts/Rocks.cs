e202f8594420a9cdf55205458e183308c83b7c2f
loicrohan <loicrohan@gmail.com>
2023-07-03 12:37:16 +0530

    Game Assets

---
+++ Assets/Scripts/Rocks.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocks : MonoBehaviour
+{
+    public float speed = 5f;
+
+    public int lifetime = 5;
+
+    [SerializeField]
+    private AudioClip _clip;
+
+    private void Start()
+    {
+        Invoke("DestroyThis", lifetime);
+    }
+
+    void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+
+    void DestroyThis()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            AudioSource.PlayClipAtPoint(_clip, Camera.main.transform.position, 1f);
+            PlayerPlane.score++;
+        }
+    }
+}
